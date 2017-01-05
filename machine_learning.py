59155c9ad8659d1fb7e742a38a0b4c3151b0d719
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-01-06 01:13:51 +0300

    changes

--- machine_learning.py
+++ machine_learning.py
@@ -1,9 +1,12 @@
 #Import required library
 import pandas
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_predict
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import SGDRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 #Read in the data.
 movies = pandas.read_csv("movie_metadata.csv")
@@ -15,13 +18,13 @@ movies = movies[movies["gross"] > 0]
 movies = movies.dropna(axis=0)
 
 #find correlation
-#print movies.corr()["gross"]
+print movies.corr()["gross"]
 
 #Get all the columns from the dataframe.
 columns = movies.columns.tolist()
 
 # Filter the columns to remove ones we don't want.
-columns = [c for c in columns if c in ["num_voted_users"]]#,"num_user_for_reviews", "num_critic_for_reviews"]]
+columns = [c for c in columns if c in ["num_voted_users", "num_user_for_reviews", "num_critic_for_reviews"]]
 
 # Store the variable we'll be predicting on.
 target = "gross"
@@ -33,7 +36,7 @@ train = movies.sample(frac=0.8, random_state=1)
 test = movies.loc[~movies.index.isin(train.index)]
 
 # Initialize the model class.
-model = SGDRegressor(shuffle=True, n_iter=200)
+model = LinearRegression()
 
 # Fit the model to the training data.
 model.fit(train[columns], train[target])
@@ -43,3 +46,4 @@ predictions = model.predict(test[columns])
 
 # Compute error between our test predictions and the actual values.
 print mean_squared_error(predictions, test[target])
+
